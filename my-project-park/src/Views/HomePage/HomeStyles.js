2e64eee3c226c17452e7ebfa4156c5d576766ed1
amandasantiagu <amandynhah023@hotmail.com>
2021-11-15 14:31:12 -0300

    Criaçao de telas, instalção do styled component e router

---
+++ my-project-park/src/Views/HomePage/HomeStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HomeStyle = styled.main`
+    width: 100%;
+    height: auto;
+    min-height: 100vh;
+    max-width: 100vw;
+`
+export const Title = styled.title`
+   font-size: 1rem;
+   
+`
